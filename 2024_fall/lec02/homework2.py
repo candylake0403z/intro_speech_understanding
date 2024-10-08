91c66c09a7c397e40bb24bf731a4007dbdab9189
candylake0403z <103738862+candylake0403z@users.noreply.github.com>
2024-10-08 10:07:13 +0900

    Update homework2.py

--- 2024_fall/lec02/homework2.py
+++ 2024_fall/lec02/homework2.py
@@ -1,18 +1,3 @@
-
-a = None
-b = None
-c = None
-name1 = None
-name2 = None
-d = None
-e = None
-f = None
-g = None
-h = None
-i = None
-j = None
-k = None
-
 # 1. Variable a: Assign a string
 a = "This is a python string"
 
