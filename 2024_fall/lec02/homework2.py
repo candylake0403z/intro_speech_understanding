63bc84132bcbfda9e55ccf49439449859e651198
candylake0403z <103738862+candylake0403z@users.noreply.github.com>
2024-10-08 10:06:37 +0900

    Update homework2.py

--- 2024_fall/lec02/homework2.py
+++ 2024_fall/lec02/homework2.py
@@ -12,3 +12,38 @@ h = None
 i = None
 j = None
 k = None
+
+# 1. Variable a: Assign a string
+a = "This is a python string"
+
+# 2. Variable b: Result of 7 % 3 (modulus operation)
+b = 7 % 3
+
+# 3. Variable c: Result of 55 * 234 (multiplication)
+c = 55 * 234
+
+# 4. Variable d: Concatenate 'name1' and 'name2'
+name1 = "KCGI"
+name2 = "Kyoto"
+d = name1 + " @ " + name2
+
+# 5. Variable e: Round 3.458 to 2 decimal places
+e = round(3.458, 2)
+
+# 6. Variable f: String representation of the integer 57
+f = str(57)
+
+# 7. Variable g: Boolean result of 5 == 9 (comparison)
+g = (5 == 9)
+
+# 8. Variable h: String representation of -23.0
+h = str(-23.0)
+
+# 9. Variable i: Float representation of the boolean False
+i = float(False)
+
+# 10. Variable j: Boolean representation of the string "Python"
+j = bool("Python")
+
+# 11. Variable k: Concatenate valid variable names ('global' and '_squ')
+k = "global" + "_squ"
