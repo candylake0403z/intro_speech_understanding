8c8ff396f1ddb22a006ae29d62d9daa18f787f46
candylake0403z <103738862+candylake0403z@users.noreply.github.com>
2024-10-29 09:33:54 +0900

    homework5.py

--- 2024_fall/lec05/homework5.py
+++ 2024_fall/lec05/homework5.py
@@ -16,8 +16,9 @@ def center_of_gravity(x):
     @result:
     c (scalar): x's center of gravity
     '''
-    c = 0  # change this line
-    return c
+    indices = np.arange(len(x))
+    weighted_sum = np.dot(indices, x)
+    return weighted_sum / np.sum(x)
 
 def matched_identity(x):
     '''
@@ -30,8 +31,8 @@ def matched_identity(x):
     @result:
     I (array): a 2d numpy array: an NxN identity matrix
     '''
-    I =  0 # change this line
-    return I
+    size = len(x)
+    return np.eye(size)
 
 def sine_and_cosine(t_start, t_end, t_steps):
     '''
@@ -48,9 +49,7 @@ def sine_and_cosine(t_start, t_end, t_steps):
     x (array of length t_steps): cos(t)
     y (array of length t_steps): sin(t)
     '''
-    # change these lines
-    t = 0 
-    x = 0
-    y = 0
-    # end changes here
-    return t, x, y
+    t = np.linspace(t_start, t_end, t_steps)
+    cos_values = np.cos(t)
+    sin_values = np.sin(t)
+    return t, cos_values, sin_values
