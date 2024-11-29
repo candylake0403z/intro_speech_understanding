7aecc2534c80b3809728eb9804985a17ea63f86c
candylake0403z <103738862+candylake0403z@users.noreply.github.com>
2024-11-29 16:53:46 +0900

    homework10.py

--- 2024_fall/lec10/homework10.py
+++ 2024_fall/lec10/homework10.py
@@ -16,7 +16,11 @@ def waveform_to_frames(waveform, frame_length, step):
     For every n and t such that 0 <= t*step+n <= N-1, it should be the case that 
        frames[n,t] = waveform[t*step+n]
     '''
-    raise RuntimeError("You need to change this part")
+    num_frames = int((len(waveform) - frame_length) / step) + 1
+    frames = np.zeros((frame_length, num_frames))
+    for t in range(num_frames):
+        frames[:, t] = waveform[t * step : t * step + frame_length]
+    return frames
 
 def frames_to_stft(frames):
     '''
@@ -28,7 +32,7 @@ def frames_to_stft(frames):
     @returns:
     stft (np.ndarray((frame_length,num_frames))) - the STFT (complex-valued)
     '''
-    raise RuntimeError("You need to change this part")
+    return np.fft.fft(frames, axis=0)
 
 def stft_to_spectrogram(stft):
     '''
@@ -46,6 +50,8 @@ def stft_to_spectrogram(stft):
     np.amax(spectrogram) should be 0dB.
     np.amin(spectrogram) should be no smaller than -60dB.
     '''
-    raise RuntimeError("You need to change this part")
-
-
+    magnitude = np.abs(stft)
+    magnitude[magnitude == 0] = 1e-6  # Avoid log of zero
+    spectrogram = 20 * np.log10(magnitude / np.amax(magnitude))
+    spectrogram = np.clip(spectrogram, -60, 0)
+    return spectrogram
